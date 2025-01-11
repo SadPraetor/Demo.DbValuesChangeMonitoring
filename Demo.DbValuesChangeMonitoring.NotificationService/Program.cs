2332e402f457c485f3a0e6c8cd7c16f45fc62eaa
SadPraetor <SadPraetor@gmail.com>
2025-01-11 15:20:59 +0100

    service defaults

--- Demo.DbValuesChangeMonitoring.NotificationService/Program.cs
+++ Demo.DbValuesChangeMonitoring.NotificationService/Program.cs
@@ -1,7 +1,12 @@
+using Demo.DbValuesChangeMonitoring.Data;
 using Demo.DbValuesChangeMonitoring.NotificationService;
+using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+builder.AddServiceDefaults();
+
+builder.AddSqlServerDbContext<ConfigurationContext>("ValuesChangedMonitoring");
 
 var host = builder.Build();
 host.Run();
