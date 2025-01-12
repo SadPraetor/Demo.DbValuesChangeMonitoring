dd099127cdf3544ffe8b5d5858b5affca124cc7b
SadPraetor <SadPraetor@gmail.com>
2025-01-13 00:46:07 +0100

    added test sql script

---
+++ Demo.DbValuesChangeMonitoring.NotificationService/testSQL.sql
@@ -0,0 +1,20 @@
+﻿update
+configuration.ConfigurationValues
+set
+[Value] = 22
+where
+[Name] = 'test3'
+
+select 
+*
+from
+configuration.ConfigurationValues
+
+SELECT 
+    conversation_handle,
+    message_type_name,
+    CAST(message_body AS NVARCHAR(MAX)) AS message_body_text,
+    service_name,
+    GETDATE() AS peek_time
+FROM 
+    ValuesChangeEventQueue;
