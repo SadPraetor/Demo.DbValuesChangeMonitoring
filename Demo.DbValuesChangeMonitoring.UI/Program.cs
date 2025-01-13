d61a3c22ee9b791dd4504b9cd412bd9983b74b0a
SadPraetor <SadPraetor@gmail.com>
2025-01-13 22:38:01 +0100

    adding UI project, retrieving data tested

---
+++ Demo.DbValuesChangeMonitoring.UI/Program.cs
@@ -0,0 +1,37 @@
+using Demo.DbValuesChangeMonitoring.Data;
+using Demo.DbValuesChangeMonitoring.UI.Components;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddDbContextFactory<ConfigurationContext>(opt => 
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("ValuesChangedMonitoring")));
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
